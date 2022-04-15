9266e61cca8041d8c7a6e08e5a4a62a98233c248
Lars Søndergård Petersen <trezum@gmail.com>
2022-04-15 20:47:01 +0200

    readding joblib

--- src/problems/runner.py
+++ src/problems/runner.py
@@ -2,7 +2,7 @@ import time
 from os import path
 
 
-#import joblib
+import joblib
 
 from AckleyProblem import Ackley
 
@@ -27,28 +27,27 @@ printTimer("optuna import done")
 
 def objective(trial):
     problem = Ackley()
-    x = trial.suggest_uniform("x",-32.768, 32.768)
-    y = trial.suggest_uniform("y",-32.768, 32.768)
+    x = trial.suggest_float("x",-32.768, 32.768)
+    y = trial.suggest_float("y",-32.768, 32.768)
     return problem.eval(x, y)
 
 def optuna_search():
-    printTimer("Starting search")
-    filename = "Ackley_study.pkl"
+    """A function to start the search."""
+    filename = "RandomForestClassifier_study.pkl"
+
     study = None
 
-    # if path.exists(filename):
-    #     #study = joblib.load(filename)
-    #     print("Study loaded.")
-    # else:
+    if path.exists(filename):
+        study = joblib.load(filename)
+        print("Study loaded.")
+    else:
+        study = optuna.create_study(direction="minimize", sampler=optuna.samplers.CmaEsSampler())
+        print("Study created.")
 
-
-    study = optuna.create_study(direction="maximize")
-    print("Study created.")
-
-    #for x in range(100):
-    study.optimize(objective, n_trials=10)
-    #joblib.dump(study, filename)
-    print("Study saved.")
+    for x in range(1):
+        study.optimize(objective, n_trials=100)
+        joblib.dump(study, filename)
+        print("Study saved.")
 
     # joblib.dump(study, filename)
     print("Optuna search done...")
